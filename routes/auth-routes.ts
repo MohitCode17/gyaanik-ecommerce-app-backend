b64e1ca94ff404132fe041d1c19085caac7d3674
MohitCode17 <mohit.codes17@gmail.com>
2025-04-29 22:27:22 +0530

    add google auth

--- routes/auth-routes.ts
+++ routes/auth-routes.ts
@@ -1,6 +1,9 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import * as authController from "../controllers/auth-controller";
 import { authenticate } from "../middlewares/authenticate";
+import passport from "passport";
+import { IUSER } from "../models/user-models";
+import { generateToken } from "../utils/generateToken";
 
 const router = Router();
 
@@ -24,5 +27,36 @@ router.get("/logout", authController.logout);
 
 // AUTHENTICATE USER
 router.get("/verify-auth", authenticate, authController.checkUserAuth);
+
+router.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: `${process.env.FRONTEND_URL}`,
+    session: false,
+  }),
+
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const user = req.user as IUSER;
+      const accessToken = generateToken(user);
+
+      res.cookie("access_token", accessToken, {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000,
+      });
+
+      res.redirect(`${process.env.FRONTEND_URL}`);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 export default router;
